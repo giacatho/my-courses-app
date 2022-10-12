c4a2bd564473adcf87c77052e9b85be554c8f03a
Tin Nguyen <giacatho@gmail.com>
2022-10-12 23:06:33 +0800

    Component hierarchy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
-import Course from './components/Course';
 import ListCourse from './components/ListCourse';
 
 function App() {
-  const myCourse = {
-    code: "OOP",
+  const myCourseList = [{
+    id: 1,
+    code: "FOPCS",
+    name: "Fundamental of Programming in C Sharp",
+    description: "FOPCS description"
+  }, {
+    id: 2,
+    code: "OOPCS",
     name: "Object Oriented Programming",
-    description: "Object Oriented Programming description"
-  };
+    description: "OOPCS description"
+  }];
 
   return (
     <div>
-      <ListCourse />
+      <h2>My course list</h2>
+      <ListCourse myCourseList={myCourseList} />
     </div>
   );
 }
--- src/components/ListCourse.js
+++ src/components/ListCourse.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import Course from './Course'
 
-export default function ListCourse() {
-  const myCourse1 = {
-    id: 1,
-    code: "FOPCS",
-    name: "Fundamental of Programming in C Sharp",
-    description: "FOPCS description"
-  };
-
-  const myCourse2 = {
-    id: 2,
-    code: "OOPCS",
-    name: "Object Oriented Programming",
-    description: "OOPCS description"
-  };
+export default function ListCourse({myCourseList}) {
+  const listItems = myCourseList.map((myCourse) =>
+    <div key={myCourse.id}>
+      <Course myCourse={myCourse} />
+    </div>
+  );
 
   return (
     <div>
-      <Course myCourse={myCourse1} />
-      <div></div>
-      <Course myCourse={myCourse2} />
+      {listItems}
     </div>
   )
 }
+
