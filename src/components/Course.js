194998086620bcbbbabc217ba4e01370755f6744
giacatho <giacatho@gmail.com>
2022-10-13 11:36:06 +0800

    Course without details

--- src/components/Course.js
+++ src/components/Course.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Course({myCourse}) {
   return (
     <div>
-      <div>Code: {myCourse.code}</div>
-      <div>Name: {myCourse.name}</div>
-      <div>Description: {myCourse.description}</div>
+      <div>{myCourse.name} <button>Details</button></div>
     </div>
   )
 }
